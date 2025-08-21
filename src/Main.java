53af714deaa0c1ab50c39a83c27a74445444ff14
Omar Hossam <omartemhosma7@gmail.com>
2025-08-21 22:08:05 +0300

    Revamped the guessing game with the core mechanics to work.

--- src/Main.java
+++ src/Main.java
@@ -2,64 +2,58 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
+    final static Scanner x = new Scanner(System.in);
+    final static Random rand = new Random();
+    static int numberToGuess;
+    static int guess;
+    static int attempts = 0;
+
     public static void main(String[] args) {
-        Scanner x = new Scanner(System.in);
-        Random rand = new Random();
-        boolean firstgame = true;
-
-        while (true) {
-            int numberToGuess = rand.nextInt(100) + 1;
-            int guess;
-            int attempts = 0;
-
-            if (firstgame) {
-                System.out.println("=================================");
-                System.out.println("Welcome To Number Guessing Game!");
-                System.out.println("=================================");
-                firstgame = false;
-            } else {
-                System.out.println("=================================");
-                System.out.println("New Round! Try to Beat Me Again.");
-                System.out.println("=================================");
-            }
-
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            System.out.println("I have chosen a number between 1 and 100. Try to guess it!");
-
-            while (true) {
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.print("Enter Your Guess: ");
-                guess = x.nextInt();
-                attempts++;
-
-                if (guess < numberToGuess) {
-                    System.out.println("Low! Try Again.");
-                } else if (guess > numberToGuess) {
-                    System.out.println("High! Try Again.");
-                } else {
-                    System.out.println("Correct! You Guessed It In " + attempts + " Tries!");
-                    break;
-                }
-            }
-
-            System.out.print("Do You Want To Play Again? (y/n): ");
-            char choice = x.next().toLowerCase().charAt(0);
-
-            if (choice != 'y') {
-                System.out.println("Thanks For Playing!");
-                break;
-            }
-        }
-
-        x.close();
+        System.out.println("=================================");
+        System.out.println("Welcome To Number Guessing Game!");
+        System.out.println("=================================");
+        gameExecute();
     }
+
+
+    public static void guessStart() {
+        System.out.print("Enter Your Guess: ");
+        guess = x.nextInt();
+        attempts++;
+
+        if (guess < numberToGuess) {
+            System.out.println("Low! Try Again.");
+        } else if (guess > numberToGuess) {
+            System.out.println("High! Try Again.");
+        } else {
+            System.out.println("Correct! You Guessed It In " + attempts + " Tries!");
+            retrycheck();
+            return;
+        }
+        guessStart();
+    }
+
+    public static void retrycheck() {
+        System.out.print("Do You Want To Play Again? (y/n): ");
+        if (x.next().toLowerCase().charAt(0) != 'y') {
+            System.out.println("Thanks For Playing!");
+            x.close();
+            return;
+        }
+        gameExecute();
+    }
+
+    public static void gameExecute() {
+        if (attempts != 0) {
+            System.out.println("=================================");
+            System.out.println("New Round! Try to Beat Me Again.");
+            System.out.println("=================================");
+            attempts = 0;
+        }
+        numberToGuess = rand.nextInt(100) + 1;
+        System.out.println("I have chosen a number between 1 and 100. Try to guess it!");
+        guessStart();
+    }
+
 }
